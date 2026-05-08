ac2f20542da86e178fcbca34450b0ac1ad6efe19
Vaz-Dev <lucasvazdev@gmail.com>
2026-05-08 00:16:24 -0300

    fix: fixed broken busy wait

--- src/input.rs
+++ src/input.rs
@@ -1,10 +1,15 @@
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use std::time::Duration;
 
 pub fn input() -> KeyCode {
     loop {
-        match event::read().unwrap() {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => key_event.code,
-            _ => continue,
-        };
+        if event::poll(Duration::from_millis(16)).unwrap() {
+            match event::read().unwrap() {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    return key_event.code
+                }
+                _ => continue,
+            }
+        }
     }
 }
